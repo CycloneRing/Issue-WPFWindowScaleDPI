a2876086553969591099c0596089cc16ad056eb4
Cyclone <91777373+CycloneRing@users.noreply.github.com>
2023-11-13 18:18:40 +0330

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfDpiTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        protected override void OnDpiChanged(DpiScale oldDpi, DpiScale newDpi)
+        {
+            MessageBox.Show($"dpi changed from {oldDpi.DpiScaleX} to {newDpi.DpiScaleX}");
+            base.OnDpiChanged(oldDpi, newDpi);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
